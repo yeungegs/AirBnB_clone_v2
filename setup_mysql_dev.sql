0f6193edbab6ecb71998e4579d8cb93ff7d21844
kedpak <kedpak@yahoo.com>
2017-08-14 20:58:30 +0000

    changed syntax

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,7 +1,5 @@
 -- creates database and user
--- grants all priveleges for database hbnb_dev_db
--- grants select priveleges to database performance_schema
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL PRIVELEGES ON hbnb_dev_db. * TO 'hbnb_dev'@'localhost';
-GRANT SELECT ON performance_schema. * TO 'hbnb_dev'@'localhost';
+GRANT ALL PRIVELEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
