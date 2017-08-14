78b3343d0864cc161c973a66de7ae63c88c8ff9b
kedpak <kedpak@yahoo.com>
2017-08-14 01:18:49 +0000

    modified attributes for sqlalchemy

--- models/city.py
+++ models/city.py
@@ -8,9 +8,11 @@ from models.base_model import BaseModel
 
 class City(BaseModel):
     """City class handles all application cities"""
-
-    state_id = ''
-    name = ''
+    
+    __tablename__ = 'cities'
+    name = Column(String(128), nullable=False)
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    places = relationship('Place', backref="cities")
 
     def __init__(self, *args, **kwargs):
         """instantiates a new city"""
