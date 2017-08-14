123b578f5fbbf3911cd932c9454a5fc8e2ae8b31
kedpak <kedpak@yahoo.com>
2017-08-14 20:55:27 +0000

    deleted redundant code

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -2,8 +2,6 @@
 -- grants all priveleges for database hbnb_dev_db
 -- grants select priveleges to database performance_schema
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-DROP USER 'hbnb_dev'@'localhost'; -- reference bug, must drop and flush before create user
-FLUSH PRIVILEGES;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVELEGES ON hbnb_dev_db. * TO 'hbnb_dev'@'localhost';
 GRANT SELECT ON performance_schema. * TO 'hbnb_dev'@'localhost';
