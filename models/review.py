6934d8e0b5449b6bbabe6a98bd7344fd0fe24750
kedpak <kedpak@yahoo.com>
2017-08-14 01:23:34 +0000

    modified reviews attributes to utilize sqlchemy

--- models/review.py
+++ models/review.py
@@ -9,9 +9,10 @@ from models.base_model import BaseModel
 class Review(BaseModel):
     """Review class handles all application reviews"""
 
-    place_id = ''
-    user_id = ''
-    text = ''
+    __tablename__ = 'reviews'
+    place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+    text = Column(String(1024),nullable=False)
 
     def __init__(self, *args, **kwargs):
         """instantiates a new review"""
