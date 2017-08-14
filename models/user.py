bb56f3ab830eb130f1cbd1a2b123bff301ac0669
kedpak <kedpak@yahoo.com>
2017-08-14 01:32:48 +0000

    modified user attributes to utilize sqlalchemy

--- models/user.py
+++ models/user.py
@@ -9,10 +9,13 @@ from models.base_model import BaseModel
 class User(BaseModel):
     """User class handles all application users"""
 
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+    __tablename__ = 'users'
+    email = Column(String(128),nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=False)
+    last_name = Column(String(128), nullable=False)
+    places = relationship('Place'. backref='user')
+    reviews = relationship('Review', backref='user')
 
     def __init__(self, *args, **kwargs):
         """instantiates a new user"""
