c6773961146533a1b95dbb64b2fda803d443bf95
kedpak <kedpak@yahoo.com>
2017-08-14 01:50:15 +0000

    modified place attributes to utilize sqlalchemy

--- models/place.py
+++ models/place.py
@@ -9,17 +9,26 @@ from models.base_model import BaseModel
 class Place(BaseModel):
     """Place class handles all application places"""
 
-    city_id = ''
-    user_id = ''
-    name = ''
-    description = ''
-    number_rooms = 0
-    number_bathrooms = 0
-    max_guest = 0
-    price_by_night = 0
-    latitude = 0.0
-    longitude = 0.0
+     place_amenity = Table("place_amenity", metadata = Base.metadata,
+                          Column('place_id', String(60), ForeignKey('places.id'),
+                                 primary_key=True, nullable=False),
+                          Column('amenity_id', String(60), ForeignKey('amenities.id'),
+                                 primary_key=True, nullable=False))
+
+    __tablename__ = 'places'
+    city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+    name = Column(String(128), nullable=False)
+    description = Column(String(1024), nullable=False)
+    number_rooms = Column(Integer, default=0, nullable=False)
+    number_bathrooms = Column(Integer, default=0, nullalbe=False)
+    max_guest = Column(Integer, default=0, nullable=False)
+    price_by_night = Column(Integer, default=0, nullable=False)
+    latitude = Column(Float, nullable=False)
+    longitude = Column(Float, nullable=False)
     amenity_ids = ['', '']
+    amenities = relationship('Amenity', secondary='place_amenity', viewonly=False)
+    reviews = relationship('Review', backref='place')
 
     def __init__(self, *args, **kwargs):
         """instantiates a new place"""
