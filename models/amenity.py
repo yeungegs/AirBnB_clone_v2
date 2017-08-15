5b37d5025099a3e5b78a62dea38b174e5473cf5a
kedpak <kedpak@yahoo.com>
2017-08-15 04:53:59 +0000

    inherted and imported base

--- models/amenity.py
+++ models/amenity.py
@@ -3,14 +3,14 @@
 Amenity Class from Models Module
 """
 
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 import sqlalchemy
 from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 
-class Amenity(BaseModel):
+class Amenity(BaseModel, Base):
     """Amenity class handles all application amenities"""
     
     __tablename__ = 'amenities'
