c26dbbbe84517b56d6065ed871f65f4b72e01682
Elaine Yeung <yeungegs@users.noreply.github.com>
2017-08-14 20:21:37 -0700

    Update __init__ file

--- models/__init__.py
+++ models/__init__.py
@@ -1,13 +1,23 @@
-from models.engine import file_storage
-from models.base_model import BaseModel
+#!/usr/bin/python3
+import os
+from models.base_model import BaseModel, Base
 from models.amenity import Amenity
 from models.city import City
 from models.place import Place
 from models.review import Review
 from models.state import State
 from models.user import User
+from models.engine import file_storage, db_storage
 
-storage = file_storage.FileStorage()
+try:
+    if os.environ['HBNB_TYPE_STORAGE'] == "db":
+        storage = db_storage.DBStorage()
+    else:
+        raise KeyError
+except KeyError:
+    storage = file_storage.FileStorage()
+
 storage.reload()
+
 """CNC - dictionary = { Class Name (string) : Class Type }"""
 CNC = file_storage.FileStorage.CNC
