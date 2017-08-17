93411243cc4e24d5d8f58781745948540c058a77
Elaine Yeung <yeungegs@users.noreply.github.com>
2017-08-16 17:30:37 -0700

    Add test_db_storage - working version

---
+++ tests/test_models/test_engine/test_db_storage.py
@@ -0,0 +1,69 @@
+import unittest
+import os.path
+from os import getenv
+from datetime import datetime
+from models.base_model import Base
+from models.amenity import Amenity
+from models.engine.db_storage import DBStorage
+from models.state import State
+from models import *
+
+
+@unittest.skipIf(os.getenv('HBNB_TYPE_STORAGE', 'fs') != 'db', "db")
+class Test_DBStorage(unittest.TestCase):
+    """Test the database storage class
+    """
+    @classmethod
+    def setUpClass(cls):
+        """create a session"""
+        storage._DBStorage__session.close()
+        cls.test_dbstorage = DBStorage()
+        test_args = {'updated_at': datetime.datetime(2017, 8, 16, 21, 51, 33, 669555),
+                     'id': "0234",
+                     'created_at': datetime.datetime(2017, 8, 16, 21, 53, 26, 563266)
+                     'name': 'dogs'}
+        cls.model = Amenity(**test_args)
+        cls.store.reload()
+        cls.test_len = 0
+
+    def test_all(self):
+        result = self.test_dbstorage.all('Amenity')
+        self.assertEqual(len(result), self.test_len)
+
+    def test_all_blank(self):
+        result = self.test_dbstorage.all('')
+        self.assertEqual(result, [])
+
+    def test_all_user(self):
+        result = self.test_dbstorage.all(self, cls=User)
+        self.assertEqual(result, [])
+        self.assertEqual(len(results), self.test_len)
+
+    def test_all_state(self):
+        result = self.test_dbstorage.all('Amenity')
+        result = all('', False)
+        self.assertEqual(result, [])
+        
+    def test_all_city(self):
+        result = self.test_dbstorage.all('Amenity')
+        result = all('', details=True)
+        self.assertEqual(result, [])
+
+    def test_all_amenity(self):
+        result = self.test_dbstorage.all('Amenity')
+        self.assertEqual(len(result), self.test_len)
+        
+        
+
+    def test_all_place(self):
+        result = all('', False, True)
+        self.assertEqual(results, [])
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.test_dbstorage._DBStorage__session.close()
+        storage.reload()
+
+if __name__ == "__main__":
+    unittest.main()
+    
