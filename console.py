51f5deb64dccfb82d83a2006e39a0db6b0d8f6a5
kedpak <kedpak@yahoo.com>
2017-08-17 01:42:48 +0000

    fix console bug dealing with floats

--- console.py
+++ console.py
@@ -122,37 +122,36 @@ class HBNBCommand(cmd.Cmd):
                     my_obj = v()
             arg.pop(0) # deletes first element of array of args
             
-            try:
-                new_dict = dict(s.split('=') for s in arg) # returns new dict with key value pairs from args
-                for key, value in new_dict.items():
-                    str_value = value.strip('"').replace('_', ' ') # strip quotes
 
-                    flag = 1; # flag for checking negative int or float
+            new_dict = dict(s.split('=') for s in arg) # returns new dict with key value pairs from args
+            for key, value in new_dict.items():
+                str_value = value.strip('"').replace('_', ' ') # strip quotes
 
-                    if '.' in str_value: # checks if str_value is a float
-                        temp = str_value.split('.')
-                        if '-' in temp[0]:  # if negative inside string strips
-                            temp[0] = temp[0].strip('-')
-                            flag = 2
-                            if temp[0].isdigit() is True and temp[1].isdigit() is True: # check if digit
-                                str_value = float(str_value) 
-                                if flag == 2:
-                                    str_value = -abs(str_value)
-                                else:
-                                    str_value = float(str_value)
-                    elif '-' in str_value:
-                        str_value = str_value.strip('-')
+                flag = 1; # flag for checking negative int or float
+
+                if '.' in str_value: # checks if str_value is a float
+                    temp = str_value.split('.')
+                    if '-' in temp[0]:  # if negative inside string strips
+                        temp[0] = temp[0].strip('-')
                         flag = 2
-                    if str_value.isdigit() is True:   # checks if str_value is int
-                        str_value = int(str_value)
+                    if temp[0].isdigit() is True and temp[1].isdigit() is True: # check if digit
+                        str_value = float(str_value)
                         if flag == 2:
                             str_value = -abs(str_value)
-                    new_dict[key] = str_value
-                my_obj.__dict__.update(new_dict)   # updates dictionary with new values pairs
-                my_obj.save()
-                BaseModel(**my_obj.__dict__)
-            except:
-                pass
+                        new_dict[key] = str_value
+                        break
+                elif '-' in str_value:
+                    str_value = str_value.strip('-')
+                    flag = 2
+                if str_value.isdigit() is True:   # checks if str_value is int
+                    str_value = int(str_value)
+                    if flag == 2:
+                        str_value = -abs(str_value)
+
+                new_dict[key] = str_value
+            my_obj.__dict__.update(new_dict)   # updates dictionary with new values pairs
+            my_obj.save()
+            BaseModel(**my_obj.__dict__)
                 
             print(my_obj.id)
         
