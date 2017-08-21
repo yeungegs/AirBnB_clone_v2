370027768482560af931543d230365d900f52f3b
Elaine Yeung <yeungegs@users.noreply.github.com>
2017-08-21 16:23:57 -0700

    Remove extra space from brace expansion

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,14 +2,14 @@
 # Write a Bash script that sets up your web servaers for the deployment of web_static. It must:
 # Install Nginx if it not already installed
 sudo apt-get update
-sudo apt-get -y install nginx
+sudo apt-get -q -y install nginx
 sudo service nginx start
 # Create the folder /data/ if it doesn't already exist
 # Create the folder /data/web_static/ if it doesn't already exist
 # Create the folder /data/web_static/releases/ if it doesn't already exist
 # Create the folder /data/web_static/shared/ if it doesn't already exist
 # Create the folder /data/web_static/releases/test/ if it doesn't already exist
-sudo mkdir -p /data/web_static/{releases, shared/test}
+sudo mkdir -p /data/web_static/{releases,shared/test}
 # Create a fake HTML file /data/web_static/releases/test/index.html (with simple content, to test your Nginx configuration)
 sudo echo "<html>
     <head>
