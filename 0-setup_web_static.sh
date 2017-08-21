595cd8f92c34b1b7ca9e7e709c1d0484ed335cfa
Elaine Yeung <yeungegs@users.noreply.github.com>
2017-08-21 15:57:36 -0700

    Add web_static scipt - work in progress

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,1 +1,19 @@
 #!/usr/bin/env bash
+# Write a Bash script that sets up your web servaers for the deployment of web_static. It must:
+# Install Nginx if it not already installed
+sudo apt-get update
+sudo apt-get -y install nginx
+sudo service nginx start
+# Create the folder /data/ if it doesn't already exist
+# Create the folder /data/web_static/ if it doesn't already exist
+# Create the folder /data/web_static/releases/ if it doesn't already exist
+# Create the folder /data/web_static/shared/ if it doesn't already exist
+# Create the folder /data/web_static/releases/test/ if it doesn't already exist
+sudo mkdir -p /data/web_static/{releases, shared/test}
+# Create a fake HTML file /data/web_static/releases/test/index.html (with simple content, to test your Nginx configuration)
+# Create a symbolic link /data/web_static/current linked to the /data/web_static/releases/test/ folder. If the symbolic link already exists, it should be deleted and recreated every time the script is ran.
+# Give ownership of the /data/ folder to the ubuntu user AND group (you can assume this user and group exist). This should be recursive; everything inside should be created/owned by this user/group.
+
+# Update the Nginx configuration to serve the content of /data/web_static/current/ to hbnb_static (ex: https://mydomainname.tech/hbnb_static). Don't forget to restart Nginx after updating the configuration:
+# Use alias inside your Nginx configuration
+# Your program should always exit successfully. 
