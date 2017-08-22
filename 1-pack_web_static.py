aa61878fdaae79e1670f1655d32f9d5a19ee3fa4
Elaine Yeung <yeungegs@users.noreply.github.com>
2017-08-21 19:41:21 -0700

    Add Task 1 - working version

---
+++ 1-pack_web_static.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+All files in the folder web_static must be added to the final archive
+All archives must be stored in the folder versions (your function should create this folder if it doesn't exist)
+The name of the archive created must be web_static_<year><month><day><hour><minute><second>.tgz
+The function do_pack must return the archive path if the archive has been correctly generated. Otherwise, it should return None
+"""
+from fabric.api import *
+import time
+from datetime import date
+
+
+def do_pack():
+    timestamp = time.strftime("%Y%m%d%H%M%S")
+    try:
+        local("mkdir -p versions")
+        local("tar -cvzf versions/web_static_{:s}.tgz web_static/".
+              format(timestamp))
+        return ("versions/web_static_{:s}.tgz".format(timestamp))
+    except:
+        return None
