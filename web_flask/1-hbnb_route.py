0e8cb7cd4c491221bc1a0c97c157f20731a4007b
Elaine Yeung <yeungegs@users.noreply.github.com>
2017-09-05 18:01:30 -0700

    Add Task 1 - v1

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,2 +1,21 @@
 #!/usr/bin/python3
-"""docstring"""
+"""This module uses Flask and starts a web application"""
+
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """Hello world for flask"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbhb', strict_slashes=False)
+def hbnb():
+    """display HBNB for /hbnb"""
+    return "HBNB"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
