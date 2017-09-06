f0d15a1c24debd3dde00c4f77daa76ed6fcb9854
Elaine Yeung <yeungegs@users.noreply.github.com>
2017-09-05 17:50:03 -0700

    Update task 0 with PEP8 formatting

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -5,11 +5,10 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
 
+@app.route('/')
 def hello_hbnb():
     """Hello world for flask"""
-    
     return "Hello HBNB!"
 
 if __name__ == "__main__":
