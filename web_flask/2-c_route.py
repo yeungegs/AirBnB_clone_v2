852ec2a73bc0c2c8e8f24621ab07e1aaacf05da8
Elaine Yeung <yeungegs@users.noreply.github.com>
2017-09-05 21:27:41 -0700

    Add Task 2 - v1

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,2 +1,27 @@
 #!/usr/bin/python3
-"""docstring"""
+"""This module uses Flask and starts a web application"""
+
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """Hello world for flask"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """display HBNB for /hbnb"""
+    return "HBNB"
+
+@app.route('/c/<string:text>')
+def c_route(text):
+    """display `C` followed by value of text variable
+    replace underscores with a space"""
+    return "C {:s}".format(text.replace("_", " "))
+    
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
