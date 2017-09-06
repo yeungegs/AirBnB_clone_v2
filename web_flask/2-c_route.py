71bc5ae838c73ab5659ce3f0dcdd5cde39ef766a
Elaine Yeung <yeungegs@users.noreply.github.com>
2017-09-05 21:29:36 -0700

    Add Task 2 - update PEP8 formatting

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -17,11 +17,12 @@ def hbnb():
     """display HBNB for /hbnb"""
     return "HBNB"
 
+
 @app.route('/c/<string:text>')
 def c_route(text):
     """display `C` followed by value of text variable
     replace underscores with a space"""
     return "C {:s}".format(text.replace("_", " "))
-    
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
